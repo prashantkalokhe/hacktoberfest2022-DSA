0aaf182e1b4df82fb0d3be717b6b8e059b4da875
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2022-10-04 16:24:20 +0530

    Added Linked List and some functions

--- Java/CP/LongestSubstring.java
+++ Java/CP/LongestSubstring.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class LongestSubstring {
     static int solve(String str) {
 
         if(str.length()==0)
@@ -8,11 +8,9 @@ public class Main {
         for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
         {
             Set < Character > se = new HashSet < > ();
-            for (int j = i; j < str.length(); j++) // nested loop for getting different 
-            string starting with str[i]
+            for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
             {
-                if (se.contains(str.charAt(j))) // if element if found so mark it as ans 
-                and break from the loop
+                if (se.contains(str.charAt(j))) // if element if found so mark it as ans and break from the loop
                 {
                     maxans = Math.max(maxans, j - i);
                     break;
@@ -25,8 +23,7 @@ public class Main {
 
     public static void main(String args[]) {
         String str = "takeUforward";
-        System.out.println("The length of the longest substring without repeating 
-        characters is " + solve(str));
+        System.out.println("The length of the longest substring without repeating characters is " + solve(str));
 
     }
 }
